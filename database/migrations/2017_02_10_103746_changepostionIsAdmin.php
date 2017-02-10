5022965d88d6ee3c3da49c7cd3d37606e9136158
Zoya Wahab <zoya.wahab@cygnismedia.com>
2017-02-10 18:17:28 +0500

    Initial Commit

---
+++ database/migrations/2017_02_10_103746_changepostionIsAdmin.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangepostionIsAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function($table){
+
+            $table->dropColumn('is_admin');
+
+
+        });
+         Schema::table('users', function($table){
+
+            $table->boolean('is_admin')->after('password');
+
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+         Schema::table('users', function($table){
+
+            $table->dropColumn('is_admin');
+
+
+        });
+
+    }
+}
